7bdada9abd7a9ea9db1679f56b1c21723a111cf3
aslogd <gerarddelcastillo@gmail.com>
2020-12-18 20:52:23 +0100

    add benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,17 +1,16 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-// Fast version is about an order of magnitude faster
-pub fn fast_benchmark(_c: &mut Criterion) {
-	let input = aoc::parse_input(&INPUT);
-    //c.bench_function("fast", |b| b.iter(|| aoc::solve_fast(&input, 5)));
+const INPUT: &str = "10, 10
+N
+FFFFRFFFFRFFFFRFFFFFFFFRFFFFRFFFFRFFFFFFFFRFFFFRFFFFRFFFFFFFFRFFFFRFFFFRFFFFFFFFRFFFFRFFFFRFFFFFFFFRFFFFRFFFFRFFFFFFFFRFFFFRFFFFRFFFFFFFFRFFFFRFFFFRFFFFFFFFRFFFFRFFFFRFFFFFFFFRFFFFRFFFFRFFFFFFFFRFFFFRFFFFRFFFF
+";
+
+pub fn benchmark(c: &mut Criterion) {
+	let mut input = htt::parse_input(&INPUT).unwrap();
+    c.bench_function("Simple", |b| b.iter(|| htt::solve(&mut input)));
 
 }
 
-pub fn slow_benchmark(_c: &mut Criterion) {
-	let input = aoc::parse_input(&INPUT);
-    //c.bench_function("slow", |b| b.iter(|| aoc::solve_slow(&input, 5)));
-}
-
-criterion_group!(benches, fast_benchmark, slow_benchmark);
+criterion_group!(benches, benchmark);
 criterion_main!(benches);
 
